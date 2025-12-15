dacda34104bb143dbe4b2e61d77cd9f1c04f03c6
Betty Wong <bettywong@Mac.lan>
2025-12-15 22:07:44 +0000

    added win and lose states to game logic

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,12 @@ import "./App.css";
 import Header from "./components/Header.jsx";
 import NumberTile from "./components/NumberTile.jsx";
 import { useState } from "react";
+import WinPage from "./components/WinPage.jsx";
 
 function App() {
+  const [gameStatus, setGameStatus] = useState("playing");
+  const [attempts, setAttempts] = useState(5);
+
   // randomising the number for user to guess
   const [randomNumber, setRandomNumber] = useState(() =>
     Math.floor(Math.random() * 100 + 1)
@@ -19,37 +23,44 @@ function App() {
     return tiles;
   });
 
-  // game status, status
-
-  // have one state to handle a win / lose state e.g. const [gameStatus, setGameStatus] = [""]
-  //  a. if game is won, setGameStatus will be true and therefore render win component.
-  //  b. if game is lost, setGameStatus wiil be false and therefore render lose component.
-  // 1. check if attempts is > 0, game over, render lose component, disable buttons.
-  // 2. check is equal to random number, render win component and not game page.
-  // check less than random number.
-  // check is more than random number.
-
-  const [attempts, setAttempts] = useState(3);
-
   const guessResult = (num) => {
-    if (num !== randomNumber && num >= randomNumber) {
+    if (num === randomNumber) {
+      setGameStatus("winning");
+      return;
+    } else if (num !== randomNumber && num >= randomNumber) {
       setAllTiles(allTiles.filter((value) => value < num));
       setAttempts(attempts - 1);
     } else if (num !== randomNumber && num <= randomNumber) {
       setAllTiles(allTiles.filter((value) => value > num));
       setAttempts(attempts - 1);
-    } else {
-      console.log("you win!");
     }
-  };
 
-  const anotherTest = () => {
-    return console.log("goodbye world");
+    if (attempts === 1) {
+      setGameStatus("losing");
+    }
   };
 
   const handleOnClick = (clickedNumber) => {
     guessResult(clickedNumber);
   };
+
+  if (gameStatus === "winning") {
+    return (
+      <>
+        <Header />
+        <WinPage />
+      </>
+    );
+  }
+
+  if (gameStatus === "losing") {
+    return (
+      <>
+        <Header />
+        <h1>You Lose</h1>
+      </>
+    );
+  }
 
   return (
     <>
